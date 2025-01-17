3b6f32d79d93928f16af55b64d1da91968cc5191
kaalibro <konstantin.adamovich@gmail.com>
2025-01-18 03:48:32 +0500

    Format gguf path to display filename only

--- py/defs/ext/gguf.py
+++ py/defs/ext/gguf.py
@@ -1,15 +1,18 @@
 # https://github.com/city96/ComfyUI-GGUF
 from ..meta import MetaField
-from ..formatters import calc_unet_hash
+from ..formatters import (
+    calc_unet_hash,
+    get_unet_name,
+)
 
 
 CAPTURE_FIELD_LIST = {
     "UnetLoaderGGUF": {
-        MetaField.MODEL_NAME: {"field_name": "unet_name"},
+        MetaField.MODEL_NAME: {"field_name": "unet_name", "format": get_unet_name},
         MetaField.MODEL_HASH: {"field_name": "unet_name", "format": calc_unet_hash},
     },
     "UnetLoaderGGUFAdvanced": {
-        MetaField.MODEL_NAME: {"field_name": "unet_name"},
+        MetaField.MODEL_NAME: {"field_name": "unet_name", "format": get_unet_name},
         MetaField.MODEL_HASH: {"field_name": "unet_name", "format": calc_unet_hash},
     },
 }
