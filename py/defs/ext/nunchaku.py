2f6c58bb896c730d2b961f7248d96d8eb7b5d203
kaalibro <konstantin.adamovich@gmail.com>
2025-05-16 06:37:54 +0500

    Support ComfyUI-nunchaku

---
+++ py/defs/ext/nunchaku.py
@@ -0,0 +1,19 @@
+# https://github.com/mit-han-lab/ComfyUI-nunchaku
+from ..meta import MetaField
+from ..formatters import calc_lora_hash
+
+
+CAPTURE_FIELD_LIST = {
+    "NunchakuFluxDiTLoader": {
+        MetaField.MODEL_NAME: {"field_name": "model_path"},
+    },
+    "NunchakuFluxLoraLoader": {
+        MetaField.LORA_MODEL_NAME: {"field_name": "lora_name"},
+        MetaField.LORA_MODEL_HASH: {
+            "field_name": "lora_name",
+            "format": calc_lora_hash,
+        },
+        MetaField.LORA_STRENGTH_MODEL: {"field_name": "lora_strength"},
+        MetaField.LORA_STRENGTH_CLIP: {"value": 0},
+    },
+}
