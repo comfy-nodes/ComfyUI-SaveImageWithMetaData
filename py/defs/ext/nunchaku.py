b94f05eb8b8b231e1c3519583423d48ad47d1cb6
kaalibro <konstantin.adamovich@gmail.com>
2025-08-06 23:54:24 +0500

    Update NunchakuFluxDiTLoader model name format

--- py/defs/ext/nunchaku.py
+++ py/defs/ext/nunchaku.py
@@ -1,18 +1,15 @@
 # https://github.com/mit-han-lab/ComfyUI-nunchaku
 from ..meta import MetaField
-from ..formatters import calc_lora_hash
+from ..formatters import calc_lora_hash, get_unet_name
 
 
 CAPTURE_FIELD_LIST = {
     "NunchakuFluxDiTLoader": {
-        MetaField.MODEL_NAME: {"field_name": "model_path"},
+        MetaField.MODEL_NAME: {"field_name": "model_path", "format": get_unet_name},
     },
     "NunchakuFluxLoraLoader": {
         MetaField.LORA_MODEL_NAME: {"field_name": "lora_name"},
-        MetaField.LORA_MODEL_HASH: {
-            "field_name": "lora_name",
-            "format": calc_lora_hash,
-        },
+        MetaField.LORA_MODEL_HASH: {"field_name": "lora_name", "format": calc_lora_hash},
         MetaField.LORA_STRENGTH_MODEL: {"field_name": "lora_strength"},
         MetaField.LORA_STRENGTH_CLIP: {"value": 0},
     },
